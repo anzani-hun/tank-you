ddc9e6bd73df0b0b644f279b7760fd2d1ff42567
anzani-hun <anzani@hun>
2024-03-11 17:16:47 +0100

    masodik

--- config/forms.py
+++ config/forms.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
-class RegistrationForm(forms.ModelForm):
+class RegistrationForm(UserCreationForm):
     password = forms.CharField(widget=forms.PasswordInput)
     class Meta:
         model = User
-        fields = ['username', 'password']
+        fields = ['username', 'email', 'password1', 'password2']
 
 class LoginForm(forms.Form):
     username = forms.CharField()
